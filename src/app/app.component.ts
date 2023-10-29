6be2f9cfb551a523424c4f3d4c4f8ea070322886
Rafaello Marcelino <rafaello.marcelino@gmail.com>
2023-10-29 10:20:14 -0700

    update image carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Email } from './email';
 import { EmailService } from './service/email.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { DownloadService } from './service/download.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit{
   constructor(private emailService: EmailService, private downloadService: DownloadService) {}
 
   ngOnInit(): void {
-      
+    initFlowbite();
   }
 
   public  testConnection(): void {
