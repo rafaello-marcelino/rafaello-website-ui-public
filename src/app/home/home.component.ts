426a314a8acc7c83454eb556aa2e16b8a36ae3a0
Rafaello Marcelino <rafaello.marcelino@gmail.com>
2023-10-29 22:55:29 -0700

    download resume from assets

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -41,17 +41,21 @@ export class HomeComponent {
     }
   
   public  downloadResume(): void {
-    this.downloadService.downloadResume().subscribe(
-      response => {
-        const file = response;
-        const url = window.URL.createObjectURL(new Blob([response as BlobPart], { type: 'application/pdf' }));
-        var link = document.createElement('a');
-        document.body.appendChild(link);
-        link.setAttribute('style', 'display: none');
-        link.href = url;
-        link.download = 'Rafaello Marcelino - Software Developer.pdf';
-        link.click();
-    });
+      let link = document.createElement("a");
+      link.download = "Rafaello Marcelino - Software Developer.pdf";
+      link.href = "./assets/Rafaello Marcelino - Software Developer.pdf";
+      link.click();
+  //   this.downloadService.downloadResume().subscribe(
+  //     response => {
+  //       const file = response;
+  //       const url = window.URL.createObjectURL(new Blob([response as BlobPart], { type: 'application/pdf' }));
+  //       var link = document.createElement('a');
+  //       document.body.appendChild(link);
+  //       link.setAttribute('style', 'display: none');
+  //       link.href = url;
+  //       link.download = 'Rafaello Marcelino - Software Developer.pdf';
+  //       link.click();
+  //   });
   }
 
 
