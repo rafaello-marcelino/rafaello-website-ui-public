127e603302c83fb5c2d3cab5e8b9591cb7d97468
Rafaello Marcelino <rafaello.marcelino@gmail.com>
2023-11-22 16:14:19 -0800

    fixed home button and modal for carousel

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./current.component.css']
 })
 export class CurrentComponent {
-
+  modalSource : string = '';
+  onClick(event: { target: any; srcElement: any; currentTarget: any; }){
+    const imgElem = event.target;
+    var target = event.target || event.srcElement || event.currentTarget;
+    var srcAttr = target.attributes.src;
+    this.modalSource = srcAttr.nodeValue;
+  }
 }
